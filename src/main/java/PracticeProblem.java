9bfc0e92f80c260229b56cd94e1e7d3068af4692
KavinKumaran123 <166553090+KavinKumaran123@users.noreply.github.com>
2026-02-24 23:05:20 +0000

    Review 12

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,43 @@
-import java.util.*;
+import java.util.HashMap;
+import java.util.ArrayList;
 
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
+	}
+	public static void swap(ArrayList<String> c, int a, int b){
+		String d = c.get(a);
+		c.set(a,c.get(b));
+		c.set(b,d);
+
+	}
+	public static ArrayList<Double> createArrayList(double[] a){
+    	ArrayList<Double> c = new ArrayList<>();
+
+    	for (int i = 0; i < a.length; i++){
+        	c.add(a[i]);
+    	}
+
+    	return c;
+	}
+	public static HashMap<String,Integer> combineParallelArrays(String[]a, int []b){
+		HashMap<String, Integer> c = new HashMap<>();
+		for (int i =0; i<a.length; i++){
+			c.put(a[i], b[i]);
+		}
+		return c;
+	}
+
+
+
+
+
+
+
+	public static HashMap<String, Integer> increaseAge(HashMap<String, Integer> a, String b){
+		a.put(b, a.get(b)+1);
+		return a;
 	}
 
 }
